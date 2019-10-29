6e4ed2b079a212ad192ae85e2674dc6dba5f954c
Raul Popadineti <raulpopadineti@gmail.com>
2019-10-29 12:59:21 +0200

    Google: transform product feeds file to .tsv

--- lib/spree_feeds/google.rb
+++ lib/spree_feeds/google.rb
@@ -16,48 +16,34 @@
 # - availability
 # - price
 # - item_group_id (only if the product has variants)
+require 'csv'
 
 module SpreeFeeds
   class Google < SpreeFeeds::Base
 
     def perform
-      tags = Config.google_shopping_tags
-      file_path = "#{@base_path}/google.xml"
-      file = File.new("#{file_path}.tmp", "w")
+      tags = SpreeFeeds::Config.google_shopping_tags
+      file_path = "#{@base_path}/google.tsv"
+      tmp_name = "#{file_path}.tmp"
 
-      xml = Builder::XmlMarkup.new(target: file)
-      xml.instruct! :xml, version: "1.0"
-      xml.rss version: "2.0", "xmlns:g" => "http://base.google.com/ns/1.0" do
-        xml.channel do
-          xml.title Config.google_shopping_rss_title
-          xml.description Config.google_shopping_rss_description
-          xml.link @root_url
+      options = { col_sep: '\t' }
+      CSV.open(tmp_name, "w", options) do |csv|
+        csv << tags
 
-          @products.find_each(batch_size: 200) do |product|
-            variants = product.variants.many? ? product.variants : [product.master]
-            variants.each do |variant|
-              helper = Helpers::GoogleShoppingFeed.new(variant, @root_url)
-              xml.item do
-                tags.map(&:to_sym).each do |t|
-                  if helper.respond_to?(t) && value = helper.send(t)
-                    if value.is_a?(Array)
-                      value.each { |v| xml.g t, v.to_s }
-                    elsif value.is_a?(Hash)
-                      xml.tag!('g:shipping') { value.each { |k, v| xml.g k, v } }
-                    else
-                      xml.g t, value.to_s
-                    end
-                  end
-                end
+        @products.find_each(batch_size: 200) do |product|
+          variants = product.variants.many? ? product.variants : [product.master]
+          variants.each do |variant|
+            helper = Helpers::GoogleShoppingFeed.new(variant, @root_url)
+            csv << tags.map do |tag|
+              if helper.respond_to?(tag) && value = helper.public_send(tag)
+                value
               end
             end
           end
         end
       end
 
-      file.close
-      File.rename("#{file_path}.tmp", file_path)
+      File.rename(tmp_name, file_path)
     end
-
   end
 end
